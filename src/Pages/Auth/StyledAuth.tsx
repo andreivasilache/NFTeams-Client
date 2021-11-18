2f45b254c8c724235abc69cbadf71d5d71529718
Andrei VASILACHE <andrei.vasilache@assist.ro>
2021-11-18 15:01:38 +0200

    Fixed formating

--- src/Pages/Auth/StyledAuth.tsx
+++ src/Pages/Auth/StyledAuth.tsx
@@ -13,70 +13,70 @@ const PXtoVH = (px:number) =>{
 */
 
 const StyledAuth = styled.div`
-    overflow: hidden;
-    background: linear-gradient(84.24deg, #6148D6 15.44%, #3B25B0 75.09%), #090C10;
-    height: 100vh;
-    padding-left: 14.53125vw;
-    position: relative;
-    font-family: "SF Pro Display";
+  overflow: hidden;
+  background: linear-gradient(84.24deg, #6148d6 15.44%, #3b25b0 75.09%), #090c10;
+  height: 100vh;
+  padding-left: 14.53125vw;
+  position: relative;
+  font-family: 'SF Pro Display';
 
-    .main-text,
-    .sub-text{
-      margin: 0;
-      font-style: normal;
-      font-weight: normal;
-    }
+  .main-text,
+  .sub-text {
+    margin: 0;
+    font-style: normal;
+    font-weight: normal;
+  }
 
-    .main-text{
-        padding-top: 26.39vh;
-        font-size: 32px;
-        line-height: 48px;
-    }
-    .sub-text{
-      margin-bottom: 8.3vh;
-      font-size: 20px;
-      line-height: 40px;
-    }
-    
-    .login-photo{
-      position: absolute;
-      width: 34.32vw;
-      height: 55.83vh;
-      right: 23.43vw;
-      top: 18.24vh;
-    }
+  .main-text {
+    padding-top: 26.39vh;
+    font-size: 32px;
+    line-height: 48px;
+  }
+  .sub-text {
+    margin-bottom: 8.3vh;
+    font-size: 20px;
+    line-height: 40px;
+  }
 
-    .vector-photo{
-      position: absolute;
-      width: 61.77vw;
-      height: 85.92vh;
-      right: 0px;
-      top: 0px;
-    }
+  .login-photo {
+    position: absolute;
+    width: 34.32vw;
+    height: 55.83vh;
+    right: 23.43vw;
+    top: 18.24vh;
+  }
 
-    .MuiTab-root{
-      color : #98A1C8;
-      padding: 0px;
-      margin-right: 21px;
-      text-transform: capitalize;
-      font-style: normal;
-      font-size: 14px;
-      line-height: 24px;
-    }
-    .MuiTabs-root{
-      width: 300px;
-    }
-    .MuiCheckbox-root{
-      color : #fff;
-    }
-    .login-button,
-    .register-button{
-    background-color:#7EFACD;
+  .vector-photo {
+    position: absolute;
+    width: 61.77vw;
+    height: 85.92vh;
+    right: 0px;
+    top: 0px;
+  }
+
+  .MuiTab-root {
+    color: #98a1c8;
+    padding: 0px;
+    margin-right: 21px;
+    text-transform: capitalize;
+    font-style: normal;
+    font-size: 14px;
+    line-height: 24px;
+  }
+  .MuiTabs-root {
+    width: 300px;
+  }
+  .MuiCheckbox-root {
+    color: #fff;
+  }
+  .login-button,
+  .register-button {
+    background-color: #7efacd;
     margin-top: 8.7vh;
     padding: 12px 32px;
     width: 132px;
     height: 40px;
-    color:#5F47D4;
+    color: #5f47d4;
     text-transform: capitalize;
     font-style: normal;
     font-weight: bold;
@@ -85,18 +85,18 @@ const StyledAuth = styled.div`
     border-radius: 8px;
   }
   .login-button:hover,
-  .register-button:hover{
-    background-color:#7EFACD;
+  .register-button:hover {
+    background-color: #7efacd;
   }
-    @media (max-width: 1060px) {
-      .vector-photo{
-        display: none;
-      }
-      .login-photo{
-        display: none;
-      }
+  @media (max-width: 1060px) {
+    .vector-photo {
+      display: none;
     }
-`
+    .login-photo {
+      display: none;
+    }
+  }
+`;
 
 export const authTheme = createTheme({
   palette: {
@@ -105,9 +105,7 @@ export const authTheme = createTheme({
     },
   },
   typography: {
-    fontFamily: [
-      "SF Pro Display"
-    ].join(','),
+    fontFamily: ['SF Pro Display'].join(','),
   },
 });
 
@@ -124,10 +122,10 @@ export const AuthFields = styled.div`
 export const WhiteTextField = withStyles({
   root: {
     '& .MuiInput-underline:before': {
-      borderBottomColor: '#fff'
+      borderBottomColor: '#fff',
     },
     '& .MuiInput-underline:hover:before': {
-      borderBottomColor: '#fff'
+      borderBottomColor: '#fff',
     },
   },
 })(TextField);
